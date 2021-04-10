5f3bc62f107e7b43fbbe3cff1b1b672908cc2d42
Karilla <bdelay155@gmail.com>
2021-04-10 18:41:35 +0200

    Added component

--- website/static/index.js
+++ website/static/index.js
@@ -1,7 +1,19 @@
+Vue.component('todo-item', {
+  template: '<li>Ceci est une liste</li>'
+})
+
 var app = new Vue({
     el: '#app-5',
     data: {
       message: "Hello Vue"
+    },
+    delimiters: ['[[',']]']
+  })
+
+  var app = new Vue({
+    el: '#app-6',
+    data: {
+      message: "Is this my third app"
     },
     delimiters: ['[[',']]']
   })
