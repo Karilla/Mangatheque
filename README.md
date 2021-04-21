887805eef0b9a00040ed3aab3812a71ddabbb1ed
Karilla <bdelay155@gmail.com>
2021-04-22 00:41:37 +0200

    bootstrap

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Mangatheque
 
 Je suis en train d'editer le readme
+<link rel= "stylesheet" type= "text/css" href= "{{ url_for('static',filename='css/index.css') }}">
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from app import create_app
 
-app = create_app('fjhsdekufhnsdljkfhnsdkj lfhsdbnjkfshdbéfjk')
-
 if __name__ == "__main__":
     app = create_app('fjhsdekufhnsdljkfhnsdkj lfhsdbnjkfshdbéfjk')
-    app.run()   
+    app.run(debug=True,extra_files='./static/css/index.css')  
