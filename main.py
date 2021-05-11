10280902bbc94afb366786f332a5a936b8bc4037
Karilla <bdelay155@gmail.com>
2021-05-11 22:50:32 +0200

    HEROKUUUU LETS GOOOO KAAAAdfdefefeddwdw

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import create_app
 
-app = create_app('fjhsdekufhnsdljkfhnsdkj lfhsdbnjkfshdbéfjk')
+my_app = create_app('fjhsdekufhnsdljkfhnsdkj lfhsdbnjkfshdbéfjk')
 
 if __name__ == "__main__":
-    app = create_app('fjhsdekufhnsdljkfhnsdkj lfhsdbnjkfshdbéfjk')
-    app.run(extra_files='./static/css/index.css')  
+    my_app = create_app('fjhsdekufhnsdljkfhnsdkj lfhsdbnjkfshdbéfjk')
+    my_app.run(extra_files='./static/css/index.css')  
