c7b2cdf40aa46995346c02613cbca35277cd60a3
Karilla <bdelay155@gmail.com>
2021-05-11 22:30:46 +0200

    HEROKUUUU LETS GOOOO KAAAA

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_assets import Environment, Bundle
+#from flask_assets import Environment, Bundle
 from flask_login import LoginManager
 from os import path
 
@@ -13,11 +13,11 @@ def create_app(secret_key):
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-    assets = Environment()
-    assets.init_app(app)
+    #assets = Environment()
+    #assets.init_app(app)
 
-    css = Bundle('static/css/*.css', filters='postcss', output='dist/css/main.css')
-    assets.register('css', css)
+    #css = Bundle('static/css/*.css', filters='postcss', output='dist/css/main.css')
+    #assets.register('css', css)
 
     db.init_app(app)
 
